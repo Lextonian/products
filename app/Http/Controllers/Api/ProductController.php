2f9379566a7842b7a83aa3ae503e65ae95b2a6e3
Мадиюн Тимур <chubektumoxa@gmail.com>
2025-10-17 01:39:12 +0500

    feat: создаем ресурсный контроллер для Product

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Http\Resources\ProductResource;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(Request $request)
+    {
+        $perPage = $request->get('per_page', 15);
+        $products = Product::with('category')->paginate($perPage);
+        return ProductResource::collection($products);
+    }
+
+    public function store(StoreProductRequest $request)
+    {
+        $product = Product::create($request->validated());
+        return new ProductResource($product);
+    }
+
+    public function show(Product $product)
+    {
+        return new ProductResource($product->load('category'));
+    }
+
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        $product->update($request->validated());
+        return new ProductResource($product);
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->noContent();
+    }
+}

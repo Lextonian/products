5acffdfb95f684272d39334663f27955ad65482c
Мадиюн Тимур <chubektumoxa@gmail.com>
2025-10-17 01:45:35 +0500

    chore: добавляем type hint

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,41 +3,44 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\SuccessResponse;
 use App\Models\Product;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
 use App\Http\Resources\ProductResource;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
 
 class ProductController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request): AnonymousResourceCollection
     {
         $perPage = $request->get('per_page', 15);
         $products = Product::with('category')->paginate($perPage);
         return ProductResource::collection($products);
     }
 
-    public function store(StoreProductRequest $request)
+    public function store(StoreProductRequest $request): ProductResource
     {
         $product = Product::create($request->validated());
         return new ProductResource($product);
     }
 
-    public function show(Product $product)
+    public function show(Product $product): ProductResource
     {
         return new ProductResource($product->load('category'));
     }
 
-    public function update(UpdateProductRequest $request, Product $product)
+    public function update(UpdateProductRequest $request, Product $product): ProductResource
     {
         $product->update($request->validated());
         return new ProductResource($product);
     }
 
-    public function destroy(Product $product)
+    public function destroy(Product $product): SuccessResponse
     {
         $product->delete();
-        return response()->noContent();
+        return new SuccessResponse('Продукт удалён');
     }
 }

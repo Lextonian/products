793db4a8b73249e144e216937d24d5dd45b770ce
Мадиюн Тимур <chubektumoxa@gmail.com>
2025-10-17 01:46:45 +0500

    feat: создаем ресурсы для контроллеров

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'category_id' => $this->category_id,
+        ];
+    }
+}

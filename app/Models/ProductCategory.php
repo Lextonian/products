84791540a0b545f48857d17e221ee3e1a6e6812b
Мадиюн Тимур <chubektumoxa@gmail.com>
2025-10-17 01:15:10 +0500

    feat: создаем модели для таблиц

---
+++ app/Models/ProductCategory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ProductCategory extends Model
+{
+    protected $fillable = ['title', 'parent_id'];
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(self::class, 'parent_id');
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(self::class, 'parent_id')->orderBy('title');
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class, 'category_id');
+    }
+}
